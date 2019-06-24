fd15e78b2c356433b88ea0807d957a38f8bd0838
Nguyên Vũ Văn <hoangnguyenvuive210698@gmail.com>
2019-06-24 11:02:36 +0700

    lesson12: Tao lop Data Access Layer

---
+++ NTMobileApp.Data.EF/EFUnitOfWork.cs
@@ -0,0 +1,25 @@
+﻿using NTMobileApp.Infrastructure.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NTMobileApp.Data.EF
+{
+    public class EFUnitOfWork : IUnitOfWork
+    {
+        private readonly AppDbContext _context;
+        public EFUnitOfWork(AppDbContext context)
+        {
+            _context = context;
+        }
+        public void Commit()
+        {
+            _context.SaveChanges();
+        }
+
+        public void Dispose()
+        {
+            _context.Dispose();
+        }
+    }
+}
